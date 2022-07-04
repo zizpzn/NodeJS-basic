4a56388bd0d9b03d4e06fee3a3edba274c015b29
zizpzn <zizpzn@gmail.com>
2022-07-04 13:54:02 +0700

    process

---
+++ 6_os/app.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+console.log(os.totalmem());
+console.log(os.freemem());
+console.log(os.type());
+console.log(os.userInfo());
+console.log(os.cpus());
+console.log(os.homedir());
+console.log(os.hostname());
---
+++ 7_process/app.js
@@ -0,0 +1,11 @@
+const process = require("process");
+
+console.log(process.execPath);
+console.log(process.version);
+console.log(process.pid);
+console.log(process.ppid);
+console.log(process.platform);
+console.log(process.env);
+console.log(process.uptime());
+console.log(process.cwd());
+console.log(process.cpuUsage());
